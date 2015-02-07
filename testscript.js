fa1f901a9da4a437a1f3ee88453eb8e9a4f242b2
Naomi <nhrubin@andrew.cmu.edu>
2015-02-06 21:45:16 -0500

    does a pretty good job finding comments

--- testscript.js
+++ testscript.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  $("#watch-discussion").replaceWith("<div>Hi</div>");
+  var replaceHTML = "<div>Hi</div>";
+  $("#watch-discussion").replaceWith(replaceHTML);
+  lastFound = null;
+  $("div").each(function() {
+    var regex = /(comment|disqus)+/i;
+    if (this.id.match(regex) || this.className.match(regex)) {
+      $(lastFound).replaceWith("");
+      lastFound = this;
+      console.log('matches: '+this.id+" "+this.className);
+    }
+  });
+  $(lastFound).replaceWith(replaceHTML);
 });
