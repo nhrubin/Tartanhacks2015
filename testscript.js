ebe2999461c83270718add6e2e4125860dd71c6a
unknown <ebinns@andrew.cmu.edu>
2015-02-07 11:27:59 -0500

    added formatting to quotes

--- testscript.js
+++ testscript.js
@@ -46,67 +46,81 @@ function replaceContents(newContents) {
  });
 }
 
+//quotes
 function quotesContent(callback) {
-  var xhrArray=[];
-  var resultArray=[];
-  var numQuotes = 20;
+  	var xhrArray=[];
+    var resultArray=[];
+	var numQuotes = 20;
 
-  var statechange=function(arrIndex) {
-    return function() {
-      if(xhrArray[arrIndex].readyState===4) {
-        ItemResponse(arrIndex,resultArray,xhrArray);
-        AllResponses(resultArray,xhrArray,numQuotes,callback);
+    var statechange=function(arrIndex) {
+      return function() {
+        if(xhrArray[arrIndex].readyState===4) {
+          ItemResponse(arrIndex,resultArray,xhrArray);
+          AllResponses(resultArray,xhrArray,numQuotes,callback);
+        }
       }
     }
-  }
 
-  for(var i=0; i<numQuotes; i++) {
-    var xhr = new XMLHttpRequest();
-    xhrArray[i]=xhr;
-    xhr.open("POST", "https://andruxnet-random-famous-quotes.p.mashape.com/cat=movies", true);
-    xhr.setRequestHeader("X-Mashape-Key", "12jk2SC0fumshzKBfoL1b80sFHuAp1zVJrHjsnpXTrIJDFEd3u");
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.setRequestHeader("Accept", "application/json");
-    xhrArray[i].onreadystatechange=statechange(i);
-    xhrArray[i].send();
-  }
+    for(var i=0; i<numQuotes; i++) {
+	  var xhr = new XMLHttpRequest();
+      xhrArray[i]=xhr;
+      xhr.open("POST", "https://andruxnet-random-famous-quotes.p.mashape.com/cat=movies", true);
+	  xhr.setRequestHeader("X-Mashape-Key", "12jk2SC0fumshzKBfoL1b80sFHuAp1zVJrHjsnpXTrIJDFEd3u");
+	  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	  xhr.setRequestHeader("Accept", "application/json");
+      xhrArray[i].onreadystatechange=statechange(i);
+      xhrArray[i].send();
+    }
 }
 
 function ItemResponse(idx,resultArray,xhrArray) {
   if(xhrArray[idx].status===200) {
-    var xhr = xhrArray[idx];
-    json = JSON.parse(xhr.responseText);
-    var author = json["author"];
-    var quote = json["quote"];
-    var category = json["category"]; //Movies or Famous
-    if(category.localeCompare("Movies")==0){
-      resultArray[idx] = '<p>"'+quote+'" -"'+author+'"</p>';
-    }
-    else{
-      resultArray[idx] = '<p>"'+quote+'" -'+author+'</p>';
-    }
-  }
+	    var xhr = xhrArray[idx];
+        json = JSON.parse(xhr.responseText);
+		var author = json["author"];
+		var quote = json["quote"];
+		var category = json["category"]; //Movies or Famous
+		if(category.localeCompare("Movies")==0){
+			resultArray[idx] = '<p>"'+quote+'" -<p3>'+author+'</p3></p><hr>';
+		}
+		else{
+			resultArray[idx] = '<p>"'+quote+'" -<p2>'+author+'</p2></p><hr>';
+		}
+   }
 }
 
 function AllResponses(resultArray,xhrArray,num,callback) {
   var i,isAllComplete=true,isAllCompleteSucc=true;
-  for(i=0;i<num;i++) if((!xhrArray[i])||(xhrArray[i].readyState!==4)) {
-    isAllComplete=false;
-    break;
-  }
-  if(isAllComplete) {
-    var text = '<div id="contentreplacement">';
-    for(i=0;i<resultArray.length;i++){
-      if(xhrArray[i].status==200) {
-        text += resultArray[i];
-      }
-      else{
-       text += "<p>"+statusText+"</p>";
-     }
+   for(i=0;i<num;i++) if((!xhrArray[i])||(xhrArray[i].readyState!==4)) {
+     isAllComplete=false;
+     break;
    }
-   text += "</div>";
-   callback(text);
- }
+   if(isAllComplete) {
+	 var text = '<style>@charset "utf-8";body {}'+
+'#commentreplacement {width: 100%;overflow: scroll;max-height: 400px;background-color: #FFFFFF;-webkit-box-shadow: 3px 3px #606060;box-shadow: 3px 3px #606060;border-radius: 0px 10px 10px;border: medium solid #9F9F9F;}'+
+'#commentreplacement p {border-bottom-color: #A3A3A3;border-top-color: #A3A3A3;padding-left: 10px;margin-left: 10px;margin-right: 10px;margin-bottom: 10px;margin-top: 10px;font-size: medium;line-height: 400%;text-align: center;}'+
+'#commentreplacement p p2 {font-weight: bold;}'+
+'#commentreplacement p p3 {font-style: italic;}'+
+'#commentreplacement #commentReplacementItems {padding-top: 5px;padding-bottom: 5px;padding-left: 5px;padding-right: 5px;}'+
+'#commentreplacement #commentReplacementItems h2 {display: inline-table;text-align: center;width: 100%;border-bottom-style: solid;margin-top: 0px;margin-bottom: 0px;line-height: 200%;border-bottom-color: #696969;}'+
+'#commentReplacementHeader p {background-color: #FFFFFF;width: 30%;height: 100%;margin-top: 0px;margin-bottom: 0px;padding-top: 0px;padding-bottom: 0px;line-height: 300%;font-weight: bold;font-size: medium;text-align: center;border-top-left-radius: 10px;border-top-right-radius: 10px;border-color: #9F9F9F;border-left-style: solid;border-right-style: solid;border-top-style: solid;border-left-width: medium;border-right-width: medium;border-top-width: medium;}'+
+'#commentfillerphotolist {margin: 0;padding: 0;list-style: none;}'+
+'#commentfillerphotolist li {float: left;width: 45%;margin: 2.5%;}'+
+'#commentfillerphotolist li img {width: 90%;margin-left: 5%;margin-right: 5%;}'+
+'#commentfillerphotolist li a p {margin: 0;padding: 5%;font-size: 0.75em;color: #bdc3c7;}'+
+'#commentreplacement #commentReplacementItems hr {width: 100%;color: #a9a9a9;background-color: #a9a9a9;height: 1px;}</style>';
+   	 text += '<div id="commentReplacementHeader"><p>CommentHider: Quotes</p></div><div id="commentreplacement"><div id="commentReplacementItems">';
+     for(i=0;i<resultArray.length;i++){
+		 if(xhrArray[i].status==200) {
+       		text += resultArray[i];
+		 }
+		 else{
+			text += "<p>"+statusText+"</p>";
+		 }
+     }
+	 text += "</div></div>";
+     callback(text);
+   }
 }
 
 function factsContent(callback) {
