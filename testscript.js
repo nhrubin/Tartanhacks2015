366b4e36c1f4a6e6f0361124082fd047d4fee14d
Naomi <nhrubin@andrew.cmu.edu>
2015-02-07 12:46:08 -0500

    added css on wiki stuff

--- testscript.js
+++ testscript.js
@@ -96,18 +96,7 @@ function AllResponses(resultArray,xhrArray,num,callback) {
      break;
    }
    if(isAllComplete) {
-	 var text = '<style>@charset "utf-8";body {}'+
-'#commentreplacement {width: 100%;overflow: scroll;max-height: 600px;background-color: #FFFFFF;-webkit-box-shadow: 3px 3px #606060;box-shadow: 3px 3px #606060;border-radius: 0px 10px 10px;border: medium solid #9F9F9F;}'+
-'#commentreplacement p {border-bottom-color: #A3A3A3;border-top-color: #A3A3A3;padding-left: 10px;margin-left: 10px;margin-right: 10px;margin-bottom: 10px;margin-top: 10px;font-size: medium;line-height: 400%;text-align: center;}'+
-'#commentreplacement p p2 {font-weight: bold;}'+
-'#commentreplacement p p3 {font-style: italic;}'+
-'#commentreplacement #commentReplacementItems {padding-top: 5px;padding-bottom: 5px;padding-left: 5px;padding-right: 5px;}'+
-'#commentreplacement #commentReplacementItems h2 {display: inline-table;text-align: center;width: 100%;border-bottom-style: solid;margin-top: 0px;margin-bottom: 0px;line-height: 200%;border-bottom-color: #696969;}'+
-'#commentReplacementHeader p {background-color: #FFFFFF;width: 30%;height: 100%;margin-top: 0px;margin-bottom: 0px;padding-top: 0px;padding-bottom: 0px;line-height: 300%;font-weight: bold;font-size: medium;text-align: center;border-top-left-radius: 10px;border-top-right-radius: 10px;border-color: #9F9F9F;border-left-style: solid;border-right-style: solid;border-top-style: solid;border-left-width: medium;border-right-width: medium;border-top-width: medium;}'+
-'#commentfillerphotolist {margin: 0;padding: 0;list-style: none;}'+
-'#commentfillerphotolist li {float: left;width: 45%;margin: 2.5%;}'+
-'#commentfillerphotolist li img {width: 90%;margin-left: 5%;margin-right: 5%;}'+
-'#commentreplacement #commentReplacementItems hr {width: 100%;color: #a9a9a9;background-color: #a9a9a9;height: 1px;}</style>';
+	 var text = textPrefix;
    	 text += '<div id="commentReplacementHeader"><p>CommentHider: Quotes</p></div><div id="commentreplacement"><div id="commentReplacementItems">';
      for(i=0;i<resultArray.length;i++){
 		 if(xhrArray[i].status==200) {
@@ -163,18 +152,7 @@ function FactAllResponses(resultArray,xhrArray,num,callback) {
      break;
    }
    if(isAllComplete) {
-	 var text = '<style>@charset "utf-8";body {}'+
-'#commentreplacement {width: 100%;overflow: scroll;max-height: 600px;background-color: #FFFFFF;-webkit-box-shadow: 3px 3px #606060;box-shadow: 3px 3px #606060;border-radius: 0px 10px 10px;border: medium solid #9F9F9F;}'+
-'#commentreplacement p {border-bottom-color: #A3A3A3;border-top-color: #A3A3A3;padding-left: 10px;margin-left: 10px;margin-right: 10px;margin-bottom: 10px;margin-top: 10px;font-size: medium;line-height: 400%;text-align: center;}'+
-'#commentreplacement p p2 {font-weight: bold;}'+
-'#commentreplacement p p3 {font-style: italic;}'+
-'#commentreplacement #commentReplacementItems {padding-top: 5px;padding-bottom: 5px;padding-left: 5px;padding-right: 5px;}'+
-'#commentreplacement #commentReplacementItems h2 {display: inline-table;text-align: center;width: 100%;border-bottom-style: solid;margin-top: 0px;margin-bottom: 0px;line-height: 200%;border-bottom-color: #696969;}'+
-'#commentReplacementHeader p {background-color: #FFFFFF;width: 30%;height: 100%;margin-top: 0px;margin-bottom: 0px;padding-top: 0px;padding-bottom: 0px;line-height: 300%;font-weight: bold;font-size: medium;text-align: center;border-top-left-radius: 10px;border-top-right-radius: 10px;border-color: #9F9F9F;border-left-style: solid;border-right-style: solid;border-top-style: solid;border-left-width: medium;border-right-width: medium;border-top-width: medium;}'+
-'#commentfillerphotolist {margin: 0;padding: 0;list-style: none;}'+
-'#commentfillerphotolist li {float: left;width: 45%;margin: 2.5%;}'+
-'#commentfillerphotolist li img {width: 90%;margin-left: 5%;margin-right: 5%;}'+
-'#commentreplacement #commentReplacementItems hr {width: 100%;color: #a9a9a9;background-color: #a9a9a9;height: 1px;}</style>';
+	 var text = textPrefix;
    	 text += '<div id="commentReplacementHeader"><p>CommentHider: Facts</p></div><div id="commentreplacement"><div id="commentReplacementItems">';
      for(i=0;i<resultArray.length;i++){
 		 if(xhrArray[i].status==200) {
@@ -216,19 +194,8 @@ function flickrContent(keyword, callback) {
 }
 
 function createPhotoList(responseArray, callback) {
-  var text = '<style>@charset "utf-8";body {}'+
-'#commentreplacement {width: 100%;overflow: scroll;max-height: 600px;background-color: #FFFFFF;-webkit-box-shadow: 3px 3px #606060;box-shadow: 3px 3px #606060;border-radius: 0px 10px 10px;border: medium solid #9F9F9F;}'+
-'#commentreplacement p {border-bottom-color: #A3A3A3;border-top-color: #A3A3A3;padding-left: 10px;margin-left: 10px;margin-right: 10px;margin-bottom: 10px;margin-top: 10px;font-size: medium;line-height: 400%;text-align: center;}'+
-'#commentreplacement p p2 {font-weight: bold;}'+
-'#commentreplacement p p3 {font-style: italic;}'+
-'#commentreplacement #commentReplacementItems {padding-top: 5px;padding-bottom: 5px;padding-left: 5px;padding-right: 5px;}'+
-'#commentreplacement #commentReplacementItems h2 {display: inline-table;text-align: center;width: 100%;border-bottom-style: solid;margin-top: 0px;margin-bottom: 0px;line-height: 200%;border-bottom-color: #696969;}'+
-'#commentReplacementHeader p {background-color: #FFFFFF;width: 30%;height: 100%;margin-top: 0px;margin-bottom: 0px;padding-top: 0px;padding-bottom: 0px;line-height: 300%;font-weight: bold;font-size: medium;text-align: center;border-top-left-radius: 10px;border-top-right-radius: 10px;border-color: #9F9F9F;border-left-style: solid;border-right-style: solid;border-top-style: solid;border-left-width: medium;border-right-width: medium;border-top-width: medium;}'+
-'#commentfillerphotolist {margin: 0;padding: 0;list-style: none;}'+
-'#commentfillerphotolist li {float: left;width: 45%;margin: 2.5%;}'+
-'#commentfillerphotolist li img {width: 90%;margin-left: 5%;margin-right: 5%;}'+
-'#commentreplacement #commentReplacementItems hr {width: 100%;color: #a9a9a9;background-color: #a9a9a9;height: 1px;}</style>';
-   	 text += '<div id="commentReplacementHeader"><p>CommentHider: Flickr</p></div><div id="commentreplacement"><div id="commentReplacementItems">';
+  var text = textPrefix;
+  text += '<div id="commentReplacementHeader"><p>CommentHider: Flickr</p></div><div id="commentreplacement"><div id="commentReplacementItems">';
   var photosArray = responseArray.photos.photo;
   text += '<ul id="commentfillerphotolist">';
   for (i=0; i<photosArray.length;i++) {
@@ -242,7 +209,7 @@ function createPhotoList(responseArray, callback) {
 }
 //flickr end
 
-
+//wiki start
 function wikiContent(callback) {
   var xhr = new XMLHttpRequest();
   var curUrl = window.location.href;
@@ -261,7 +228,8 @@ function intermediate(data, callback) {
 
 function processData(data, accum, callback) {
   if (data.length == 0) {
-    callback(accum);
+    text = textPrefix + '<div id="commentReplacementHeader"><p>CommentHider: Quotes</p></div><div id="commentreplacement"><div id="commentReplacementItems">' + accum + "</div></div>";
+    callback(text);
     return;
   }
   el = data[0];
@@ -286,7 +254,7 @@ function getWiki(topic, data, callback, accum, callback2) {
        thing2 = thing[key].extract;
      }
      if (thing2.length > 50 && thing2.indexOf("This is a redirect") < 0) {
-       callback(data, accum+thing2+" <a href='http://en.wikipedia.org/wiki/"+encodeURI(topic)+"' target='_blank'>Read more</a><br /><br />", callback2);
+       callback(data, accum+"<p>"+thing2+" <a href='http://en.wikipedia.org/wiki/"+encodeURI(topic)+"' target='_blank'>Read more</a></p><hr><br /><br />", callback2);
      } else {
        callback(data, accum, callback2);
      }
@@ -295,3 +263,15 @@ function getWiki(topic, data, callback, accum, callback2) {
  xhr.send();
 }
 
+var textPrefix = '<style>@charset "utf-8";body {}'+
+'#commentreplacement {width: 100%;overflow: scroll;max-height: 600px;background-color: #FFFFFF;-webkit-box-shadow: 3px 3px #606060;box-shadow: 3px 3px #606060;border-radius: 0px 10px 10px;border: medium solid #9F9F9F;}'+
+'#commentreplacement p {border-bottom-color: #A3A3A3;border-top-color: #A3A3A3;padding-left: 10px;margin-left: 10px;margin-right: 10px;margin-bottom: 10px;margin-top: 10px;font-size: medium;line-height: 400%;text-align: center;}'+
+'#commentreplacement p p2 {font-weight: bold;}'+
+'#commentreplacement p p3 {font-style: italic;}'+
+'#commentreplacement #commentReplacementItems {padding-top: 5px;padding-bottom: 5px;padding-left: 5px;padding-right: 5px;}'+
+'#commentreplacement #commentReplacementItems h2 {display: inline-table;text-align: center;width: 100%;border-bottom-style: solid;margin-top: 0px;margin-bottom: 0px;line-height: 200%;border-bottom-color: #696969;}'+
+'#commentReplacementHeader p {background-color: #FFFFFF;width: 30%;height: 100%;margin-top: 0px;margin-bottom: 0px;padding-top: 0px;padding-bottom: 0px;line-height: 300%;font-weight: bold;font-size: medium;text-align: center;border-top-left-radius: 10px;border-top-right-radius: 10px;border-color: #9F9F9F;border-left-style: solid;border-right-style: solid;border-top-style: solid;border-left-width: medium;border-right-width: medium;border-top-width: medium;}'+
+'#commentfillerphotolist {margin: 0;padding: 0;list-style: none;}'+
+'#commentfillerphotolist li {float: left;width: 45%;margin: 2.5%;}'+
+'#commentfillerphotolist li img {width: 90%;margin-left: 5%;margin-right: 5%;}'+
+'#commentreplacement #commentReplacementItems hr {width: 100%;color: #a9a9a9;background-color: #a9a9a9;height: 1px;}</style>';
