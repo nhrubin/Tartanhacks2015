9580aec4f3a090b64cc470bc61221264c614d2ee
Mopewa Ogundipe <mopewa.o@gmail.com>
2015-02-07 04:31:50 -0500

    don't need tab permission to create tabs. Popup settings link leads to fancy options page

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $("#options").click(function(){
-    chrome.tabs.create({'url': chrome.extension.getURL('options.html')});
+    chrome.tabs.create({'url':'chrome://extensions?options=bndpchigejlbifakgcdbejahlnojmocj'});
   });
 });
